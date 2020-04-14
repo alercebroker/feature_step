9624dff96f20f4968b673c569d6ce1e585283e8f
diego <diego@pop-os.localdomain>
2020-04-14 11:57:46 -0400

    fix: enabled logging debug option

--- scripts/run_step.py
+++ scripts/run_step.py
@@ -1,18 +1,16 @@
 import os
 import sys
-
 import logging
 SCRIPT_PATH = os.path.dirname(os.path.abspath(__file__))
 PACKAGE_PATH = os.path.abspath(os.path.join(SCRIPT_PATH,".."))
 
 sys.path.append(PACKAGE_PATH)
-from settings import *
+from settings import CONSUMER_CONFIG, STEP_CONFIG
 
 level = logging.INFO
-if 'LOGGING_DEBUG' in locals():
-    if LOGGING_DEBUG:
-        level=logging.DEBUG
-        logging.getLogger('matplotlib').setLevel(logging.WARNING)
+if os.getenv("LOGGING_DEBUG"):
+    level=logging.DEBUG
+    logging.getLogger('matplotlib').setLevel(logging.WARNING)
 
 logging.basicConfig(level=level,
                     format='%(asctime)s %(levelname)s %(name)s.%(funcName)s: %(message)s',
