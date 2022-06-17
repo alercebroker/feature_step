5f6dba0d682305c725c7cc0774d682efa2fd3c16
Javier <javier.arredondo.c@usach.cl>
2022-06-17 15:30:41 -0400

    feat(schema): add alert id to output schema

--- schema.py
+++ schema.py
@@ -24,6 +24,7 @@ SCHEMA = {
     "type": "record",
     "fields": [
      {"name": "aid", "type": "string"},
+     {"name": "alertId", "type": "string"},
      {"name": "candid", "type": "long"},
      {"name": "features", "type": [FEATURES, "null"]},
      {"name": "detections", "type": LIGHTCURVE}
