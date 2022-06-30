54338b53b98bbcc67672a136a16d69b418b669cd
Javier <javier.arredondo.c@usach.cl>
2022-06-30 14:34:56 -0400

    feat(metadata): add metadata required by new extractor

--- features/step.py
+++ features/step.py
@@ -58,7 +58,7 @@ class FeaturesComputer(GenericStep):
             5: "Y",
         }
 
-    def compute_features(self, detections: pd.DataFrame):
+    def compute_features(self, detections: pd.DataFrame, metadata: pd.DataFrame):
         """Compute Hierarchical-Features in detections and non detections to `dict`.
 
         **Example:**
@@ -67,9 +67,11 @@ class FeaturesComputer(GenericStep):
         ----------
         detections : pandas.DataFrame
             Detections of an object
+        metadata: pd.DataFrame
+            Metadata from detections
         """
         clean_lightcurves = self.preprocessor.preprocess(detections)
-        features = self.features_computer.compute_features(clean_lightcurves)
+        features = self.features_computer.compute_features(clean_lightcurves, metadata=metadata)
         return features
 
     def produce(self, output_messages):
@@ -85,6 +87,27 @@ class FeaturesComputer(GenericStep):
         detections = detections.rename(columns=self._rename_cols)
         detections["BAND"] = detections["BAND"].map(lambda x: self._fid_mapper[x])
         return detections
+
+    def get_metadata(self, light_curves: pd.DataFrame):
+        def _get_metadata(detections: pd.DataFrame):
+            metadata = {
+                "MWEBV": np.nan,
+                "REDSHIFT_HELIO": np.nan
+            }
+            for det in detections:
+                extra_fields = det["extra_fields"]
+                if "diaObject" in extra_fields.keys():
+                    dia_object = extra_fields["diaObject"]
+                    if dia_object:
+                        metadata = {
+                            "MWEBV": dia_object["mwebv"],
+                            "REDSHIFT_HELIO": dia_object["z_final"]
+                        }
+                        break
+            return pd.Series(metadata)
+
+        metadata_df = light_curves["detections"].apply(_get_metadata)
+        return metadata_df
 
     def prepare_message(self, features, light_curves):
         features.replace({np.nan: None}, inplace=True)
@@ -103,7 +126,8 @@ class FeaturesComputer(GenericStep):
         self.logger.info(f"Processing {len(messages)} light curves.")
         detections = self.map_detections(light_curves_dataframe)
         self.logger.info(f"A total of {len(detections)} detections in {len(light_curves_dataframe)} light curves")
-        features = self.compute_features(detections)
+        metadata = self.get_metadata(light_curves_dataframe)
+        features = self.compute_features(detections, metadata)
         self.logger.info(f"Features calculated: {len(features)}")
         output_messages = self.prepare_message(features, light_curves_dataframe)
         if self.producer:
